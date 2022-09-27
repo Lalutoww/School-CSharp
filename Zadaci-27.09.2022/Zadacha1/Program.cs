e19277b2e5e2cab60ac20d58e2d4c84d9e704528
Martin Lalutov <martinlalutov00@gmail.com>
2022-09-27 14:09:01 +0300

    Started new project

---
+++ Zadaci-27.09.2022/Zadacha1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Zadacha1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("Hello Profil!");
+             string name = "Ivan"; 
+             Console.WriteLine("Hello " + name); */
+
+            // Променлива за име
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
+
