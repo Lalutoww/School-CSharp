4f4ebf76348cdeb5969ee35a4f658075f523e6a8
Martin Lalutov <martinlalutov00@gmail.com>
2022-09-27 14:59:53 +0300

    Added third exercise
    Square Area added

--- Zadaci-27.09.2022/2-sum-of-numbers/Program.cs
+++ Zadaci-27.09.2022/2-sum-of-numbers/Program.cs
@@ -10,7 +10,15 @@ namespace _2_sum_of_numbers
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(a + b);
+            Console.WriteLine("a + b " + " = " + (a + b));
+
+            // деклариране на променлива за сума
+            int sum = a + b;
+            Console.WriteLine(a + " + " + b + " = " + sum);
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, sum);
+
+            Console.WriteLine($"{a} + {b} = {sum}");
         }
     }
 }
---
+++ Zadaci-27.09.2022/3-square-area/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3_square_area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            int a = int.Parse(Console.ReadLine());
+            int area = a * a;
+            int Perimeter = a * 4;
+            Console.WriteLine($"Area = {area}");
+            Console.WriteLine($"perimeter = {Perimeter}");
+        }
+    }
+}
+
