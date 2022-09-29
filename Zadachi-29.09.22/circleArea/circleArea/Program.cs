52656616a6b499a0e98a932b751ecee4281643f9
Martin Lalutov <martinlalutov00@gmail.com>
2022-09-29 15:17:34 +0300

    circleArea added

---
+++ Zadachi-29.09.22/circleArea/circleArea/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace circleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Програма за лице и дължина на окръжност");
+            Console.Write("r = ");
+            double r = double.Parse(Console.ReadLine());
+            double C = 2 * Math.PI * r;
+            double S = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine($"C = {C.ToString("0.00")}");
+            Console.WriteLine($"S = {S.ToString("0.00")}");
+
+
+        }
+    }
+}
+
