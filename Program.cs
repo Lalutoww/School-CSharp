1d2494a377dcf7aa8a7f78af944e0c9ef64cca82
Martin Lalutov <martinlalutov00@gmail.com>
2022-09-29 15:01:10 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace arithmeticExperiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5/3);
+            Console.WriteLine(1/2);
+            Console.WriteLine("5 % 3 = " + (5 % 3));
+            Console.WriteLine("10 % 3 = " + (10 % 3));
+            Console.WriteLine("10 / 3 = " + (10 / 3));
+            Console.WriteLine("0 / 5 = " + (0 / 5));
+            Console.WriteLine("0 % 5 = " + (0 % 5));
+            // int x = 5.0 + 3; // - gives error because x is integer
+            Console.WriteLine(5.0 + 3);
+            double y = 5 + 3;
+            Console.WriteLine(10.5 % 3);
+            Console.WriteLine(6/2*3); //  = 9 -> priority is left to right
+            Console.WriteLine(6 / (2 * 3)); // = 1 -> brackets are with highest priority
+
+        }
+    }
+}
+
