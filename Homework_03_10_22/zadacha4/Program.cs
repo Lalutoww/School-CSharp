b8b6a5c93de458ca1037ee7c417170da19d2038f
Martin Lalutov <martinlalutov00@gmail.com>
2022-10-03 20:54:06 +0300

    ex4

---
+++ Homework_03_10_22/zadacha4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace zadacha4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Architect's name: ");
+            string architectName = Console.ReadLine();
+            Console.Write("Project count: ");
+            int projectCount = int.Parse(Console.ReadLine());
+            int hoursNeeded = projectCount * 3;
+
+            Console.WriteLine($"The architect {architectName} will need {hoursNeeded} hours to complete {projectCount} project/s.");
+        }
+    }
+}
+
