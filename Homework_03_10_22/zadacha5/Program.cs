79ac62887301d37bf07e3a8d65a57ce31bd6ed70
Martin Lalutov <martinlalutov00@gmail.com>
2022-10-03 21:01:34 +0300

    Whole homework done

---
+++ Homework_03_10_22/zadacha5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace zadacha5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dog count: ");
+            int dogCount = int.Parse(Console.ReadLine());
+            Console.Write("Other animals count: ");
+            int otherAnimalsCount = int.Parse(Console.ReadLine());
+
+            double dogFoodPrice = dogCount * 2.50;
+            double otherAnimalFoodPrice = otherAnimalsCount * 4.00;
+
+            double sum = dogFoodPrice + otherAnimalFoodPrice;
+
+            Console.WriteLine($"{sum.ToString("0.00")} lv.");
+        }
+    }
+}
+
