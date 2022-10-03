1f8446adb57f56446074fb9dd3eaec7e4f9ac1e3
Martin Lalutov <martinlalutov00@gmail.com>
2022-10-03 20:49:20 +0300

    added HW

---
+++ Homework_03_10_22/zadacha3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace zadacha3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Периметър и лице на правоъгълник - калкулатор");
+            Console.Write("a = ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            double b = double.Parse(Console.ReadLine());
+
+            double perimeter = a * 2 + b * 2;
+            double area = a * b;
+
+            Console.WriteLine($"Perimeter = {perimeter}");
+            Console.WriteLine($"Area = {area}");
+        }
+    }
+}
+
