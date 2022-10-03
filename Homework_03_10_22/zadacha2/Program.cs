d610ea5a4821ba943a5d5064dd699198e771e300
Martin Lalutov <martinlalutov00@gmail.com>
2022-10-03 21:02:59 +0300

    add

---
+++ Homework_03_10_22/zadacha2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace zadacha2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Два вложени цикъла, за да може звездите да се отпчатват на нов ред
+            for (int i = 1; i <= 5; i++)
+            {
+                string row = "";
+                for (int j = 1; j <= i; j++)
+                {
+                    row += "* ";
+                }
+                Console.WriteLine(row);
+            }
+        }
+    }
+}
+
