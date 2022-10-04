4eb210395d85d3fc9392d966b7ad7c6180fb2fdf
Martin Lalutov <martinlalutov00@gmail.com>
2022-10-04 09:37:19 +0300

    Update Program.cs

--- classwork_04_10_22/flightHours/Program.cs
+++ classwork_04_10_22/flightHours/Program.cs
@@ -10,12 +10,14 @@ namespace flightHours
             int hours = int.Parse(Console.ReadLine());
             Console.Write("Minutes = ");
             int minutes = int.Parse(Console.ReadLine());
-            Console.Write("Hours = ");
+            Console.Write("Fly time = ");
             int length = int.Parse(Console.ReadLine());
             int hoursToMin = hours * 60;
             int allMin = hoursToMin + minutes + length;
             int allMinToHours = allMin / 60;
             int totalMin = minutes + length;
+            double ath = (minutes + length) / 60.0;
+            Console.WriteLine(ath);
 
             if (totalMin > 60)
             {
