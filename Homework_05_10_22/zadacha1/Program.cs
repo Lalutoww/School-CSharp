9ad7966a9099970fa7168a41f13c4c594a811f36
Martin Lalutov <martinlalutov00@gmail.com>
2022-10-05 17:18:41 +0300

    Added HW

---
+++ Homework_05_10_22/zadacha1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace zadacha1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете: x = ");
+            double x = double.Parse(Console.ReadLine());
+            double y = (29 - x) / (Math.Pow(x, 2) + 4);
+            double z = x + Math.Sqrt(6*y);
+            Console.WriteLine($"Y = {Math.Round(y,2)}");
+            Console.WriteLine($"Z = {Math.Round(z, 2)}");
+
+        }
+    }
+}
+
