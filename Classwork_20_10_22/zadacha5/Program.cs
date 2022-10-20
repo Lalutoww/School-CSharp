4cc622e5c3955ff723c4fc739d39939a45ad81c8
Martin Lalutov <martinlalutov00@gmail.com>
2022-10-20 22:20:17 +0300

    add

--- Classwork_20_10_22/zadacha5/Program.cs
+++ Classwork_20_10_22/zadacha5/Program.cs
@@ -6,7 +6,18 @@ namespace zadacha5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Hours = ");
+            int hours = int.Parse(Console.ReadLine());
+            Console.Write("Minutes = ");
+            int minutes = int.Parse(Console.ReadLine());
+            //------------------------\\
+            int hoursToMin = hours * 60;
+            int allInMin = hoursToMin + minutes + 15;
+            int allMinToHours = allInMin / 60;
+            int onlyMinutes = (minutes + 15) % 60;
+            if (allMinToHours >= 24) allMinToHours = 0;
+            if (onlyMinutes < 10) Console.WriteLine($"{allMinToHours}:0{onlyMinutes}");
+            else Console.WriteLine($"{allMinToHours}:{onlyMinutes}");
         }
     }
 }
---
+++ Classwork_20_10_22/zadacha7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace zadacha7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Кв.м Лозе: ");
+            int loze = int.Parse(Console.ReadLine());
+            Console.Write("Грозде за 1 кв. м: ");
+            double grapes = double.Parse(Console.ReadLine());
+            Console.Write("Нужни литри вино: ");
+            int litersWine = int.Parse(Console.ReadLine());
+            Console.Write("Брой работници: ");
+            int workers = int.Parse(Console.ReadLine());
+
+            double totalGrapes = loze * grapes;
+            double totalWine = 0.40 * totalGrapes / 2.5;
+            double needed = Math.Abs(totalWine - litersWine);
+            if(totalWine < litersWine)
+            {
+                Console.WriteLine($"It will be a tough winter! More {Math.Floor(needed)} liters wine needed.");
+            }
+            else
+            {
+                Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(totalWine)} liters. ");
+                Console.WriteLine($"{Math.Ceiling(needed)} liters left -> {Math.Ceiling(needed / workers)} liters per person.");
+            }
+
+        }
+    }
+}
+
