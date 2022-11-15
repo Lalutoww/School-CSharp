992a721077c464ddd6f24fe86d2ff3fa03fc972a
Martin Lalutov <martinlalutov00@gmail.com>
2022-11-15 12:58:38 +0200

    .

---
+++ Homework/Homework_15_11/Zadacha1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Zadacha1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter num: ");
+            int num = int.Parse(Console.ReadLine());
+            bool isBigger = false;
+            int sum = 1;
+
+
+            for(int i = 1; i <= num; i++)
+            {
+                int inputNum = int.Parse(Console.ReadLine());
+
+                if (inputNum > 10)
+                {
+                    isBigger = true;
+                }
+                if(i % 2 == 0)
+                {
+                    sum *= inputNum;
+                }
+            }
+
+            if (isBigger) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+
+            Console.WriteLine(sum);
+        }
+    }
+}
+
