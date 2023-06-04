9c3f07045ee0490d1e6ea38a4c46e1149e6d07a7
Martin Lalutov <martinlalutov00@gmail.com>
2023-06-04 23:31:03 +0300

    .

---
+++ Classwork/Classwork_30_05_23/Zadacha1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Zadacha1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ReadNumber(0, 100);
+    }
+
+    static void ReadNumber(int start, int end)
+    {
+        int count = 0;
+        do
+        {
+            Console.Write($"Enter number{count}: ");
+            try
+            {
+                int newNumber = int.Parse(Console.ReadLine());
+                if (newNumber < start || newNumber > end)
+                {
+                    throw new Exception("Number out of range");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            
+            count++;
+        } while (count <= 5);
+    }
+}
+
---
+++ Classwork/Classwork_30_05_23/Zadacha2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Zadacha2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter Number: ");
+        int number = int.Parse(Console.ReadLine());
+        try
+        {
+            if (number < 0) throw new Exception("The number is negative.");
+
+            double sqrtNumber = Math.Sqrt(number);
+            Console.WriteLine(sqrtNumber);
+            
+
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Good bye");
+        }
+    }
+}
+
