b29c568a7b2c1a0697e3b5b3d883b7ddd15ef670
Martin Lalutov <martinlalutov00@gmail.com>
2023-11-22 08:36:23 +0200

    add all classwork

---
+++ 12th Grade/Exams/Exam_27_09/Zadacha1/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Zadacha1;
+
+abstract class Building
+{
+    public int BuildingLevels { get; set; }
+    public string Adress { get; set; }
+
+    public Building(int buildingLevels, string adress)
+    {
+        BuildingLevels = buildingLevels;
+        Adress = adress;
+    }
+
+    public void GetAdress()
+    {
+        Console.WriteLine($"Building's adress is: {this.Adress}");
+    }
+
+    abstract public int GetHeight();
+}
+
+class House : Building
+{
+    public string Owner { get; set; }
+    public House(int buildingLevels, string adress, string owner) : base(buildingLevels, adress)
+    {
+        BuildingLevels = buildingLevels;
+        Adress = adress;
+        Owner = owner;
+    }
+
+    public override int GetHeight()
+    {
+        int height = BuildingLevels * 3 + 2;
+        return height;
+    }
+    public override string ToString()
+    {
+        return $"Adress: {Adress}; Height: {GetHeight()}" ;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n;
+        do
+        {
+            Console.Write("Enter number [1;100]: ");
+            n = int.Parse(Console.ReadLine());
+        } while (n<1 || n>100);
+
+        List<House> houseList = new List<House>();
+        for (int i = 0; i < n; i++)
+        {
+            string fix;
+            switch (i+1)
+            {
+                case 1: fix = "st"; break;
+                case 2: fix = "nd"; break;
+                case 3: fix = "rd"; break;
+                default: fix = "th"; break;
+            }
+            Console.Write($"Enter {i+1}{fix} Adress: ");
+            string adressInput = Console.ReadLine();
+            Console.Write($"Enter {i+1}{fix} building levels: ");
+            int buildingLevelsInput = int.Parse(Console.ReadLine());
+            Console.Write($"Enter {i+1}{fix} Owner: ");
+            string ownerInput = Console.ReadLine();
+
+            House newHouse = new House(buildingLevelsInput, adressInput, ownerInput);
+            houseList.Add(newHouse);
+            Console.WriteLine("------------------");
+        }
+
+        foreach (var house in houseList)
+        {
+            Console.WriteLine(house.ToString());
+        }
+    }
+}
---
+++ 12th Grade/Exams/Exam_27_09/Zadacha2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Zadacha2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //A)
+        Console.Write("Enter number: ");
+        int n = int.Parse(Console.ReadLine());
+        double[] arr = new double[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Add number: ");
+            double num = double.Parse(Console.ReadLine());
+            arr[i] = num;
+        }
+        Console.WriteLine();
+        Console.WriteLine($"Array: {String.Join(' ', arr)}");
+        //B
+        PositiveSum(arr);
+        //C
+        Console.WriteLine();
+        OddIndices(arr);
+    }
+    static void PositiveSum(double[] arr)
+    {
+        double sum = 0;
+        foreach (double number in arr)
+        {
+            if (number> 0) sum += number;
+        }
+        Console.Write($"Positive Numbers Sum: {sum}");
+    }
+    static void OddIndices(double[] arr)
+    {
+        int length = arr.Length;
+        Console.Write($"Odd indices: ");
+        for (int i = 0; i < length; i++)
+        {
+            if (i % 2 != 0) Console.Write($"{arr[i]} ");
+        }
+    }
+}
+
